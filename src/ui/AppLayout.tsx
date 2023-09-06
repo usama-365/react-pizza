fb2b846a7dc9bada293fc5493f95d10a88b12dc1
usama-365 <usama-365@protonmail.com>
2023-09-06 14:07:05 +0500

    add a loading indicator if the application state is loading

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import LoadingIndicator from "./LoadingIndicator";
 
 export default function AppLayout() {
+  const isLoading = useNavigation().state === "loading";
   return (
-    <>
+    <div className="layout">
+      {isLoading && <LoadingIndicator />}
       <Header />
       <main>
         <Outlet />
       </main>
       <CartOverview />
-    </>
+    </div>
   );
 }
---
+++ src/ui/LoadingIndicator.tsx
@@ -0,0 +1,3 @@
+export default function LoadingIndicator() {
+  return <div className="loader"></div>;
+}
